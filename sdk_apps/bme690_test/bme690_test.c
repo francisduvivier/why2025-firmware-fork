65cafbc698d9cad7e7b443bd601e569adbcaeb9b
Francis Duvivier <francisduvivier@gmail.com>
2025-08-28 19:27:20 +0200

    fix: render bme690 data as floats
    since BME68X_USE_FPU is defined

--- sdk_apps/bme690_test/bme690_test.c
+++ sdk_apps/bme690_test/bme690_test.c
@@ -16,8 +16,8 @@ int main(int argc, char *argv[]) {
     }
 
     printf("Get BME690...\n");
-    printf("Temperature in Celsius: %d \n", gas->_get_temperature(gas));
-    printf("Humidity in Rel. Percentage: %d \n", gas->_get_humidity(gas));
-    printf("Pressure in Pascal: %d \n", gas->_get_pressure(gas));
-    printf("Gas Resistance in Ohm: %d \n", gas->_get_gas_resistance(gas));
+    printf("Temperature in Celsius(.2f): %.2f \n", gas->_get_temperature(gas));
+    printf("Humidity in Rel. Percentage(.2f): %.2f \n", gas->_get_humidity(gas));
+    printf("Pressure in Pascal(.2f): %.2f \n", gas->_get_pressure(gas));
+    printf("Gas Resistance in Ohm(.2f): %.2f \n", gas->_get_gas_resistance(gas));
 }
